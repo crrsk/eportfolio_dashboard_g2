18ae19dcd196c382bbb5796efcf4b8300ddba7c5
Kike <5785235@alu.murciaeduca.es>
2025-12-08 23:34:36 +0100

    ejRefuerzo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Boton from './componentes/Boton'
-import NotaForm from './componentes/NotaForm'
+import './Botonera.css'
+import Botonera from './componentes/componentesRefuerzo/Botonera'
 
 function App() {
-  let estado=0
+  let estado = 0
+  const botonera = [{
+    texto: "LeftUp",
+    valor: 0
+  },
+  {
+    texto: "RightUp",
+    valor: 0
+  },
+  {
+    texto: "LeftDown",
+    valor: 0
+  },
+  {
+    texto: "RighDown",
+    valor: 0
+  }]
 
+ 
   return (
-    
-    <div>
-      <h1>Notas</h1>
-      <Boton estado={estado}></Boton>
-      <NotaForm></NotaForm>
+
+    <div className='botonera'>
+       
+        {botonera.map((a)=><Botonera nombre={a.texto} valor={a.valor}></Botonera> )}
+      
+      
+
     </div>
-      
-      
-    
+
+
+
   )
 }
 
