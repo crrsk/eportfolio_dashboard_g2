03d483ba9db081804d298c2669d75720b0bf18ae
Kike <5785235@alu.murciaeduca.es>
2025-12-09 09:55:57 +0100

    ejComp

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Cabecera from './componentes/Cabecera'
-import SideBar from './componentes/SideBar'
-import Main from './componentes/Main'
+import notas from './mocks/mock-notas.js'
 
 function App() {
 
- const usuario="Enrique"
- const token="dada78wda7w"
- const menu="menu"
-
-
 
   return (
-    <>
-      <div className='container-fluid'>
-        <div className="row">
-
-          <div className='col-12 cabecera'>
-            <Cabecera usuario={usuario}/>
-          </div>
-         <div className='col-3 sideBar'>
-            <SideBar menu={menu}/>
-          </div>
-          <div className='col-9 main'>
-            <Main token={token}/>
-          </div>
-        </div>
-      </div>
+    
+    <div>
       
       
-    </>
+    </div>
+      
+      
+    
   )
 }
 
